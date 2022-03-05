a3b8638328b860cb93bcc1722ede53b3f3ac479b
Luke Du <yidongd@amazon.com>
2022-03-05 09:42:22 -0800

    init

---
+++ src/test/java/com/adu21/practice2022/MainTest.java
@@ -0,0 +1,13 @@
+package com.adu21.practice2022;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void helloWorld() {
+        assertTrue(true);
+    }
+}
